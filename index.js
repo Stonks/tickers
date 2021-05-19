fdc15dfa5d22a12ca953c7788a65c0bdb37bb939
Gennaro Napolitano <gennaro@nau.edu>
2021-05-19 13:15:13 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const axios = require('axios');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+
+async function main() {
+
+    const {NYSE_API_URL, NASDAQ_API_URL} = process.env;
+
+    axios.get(NYSE_API_URL).then(async (res) => {
+        await createPromises(res.data.data.rows, 'nyse');
+    });
+
+    axios.get(NASDAQ_API_URL).then(async (res) => {
+        await createPromises(res.data.data.rows, 'nasdaq');
+    });
+
+}
+
+async function createPromises(tickers, exchange) {
+    const DATA_API_URL = process.env.DATA_API_URL;
+
+    let promises = [];
+
+    for (let i = 0; i < tickers.length; i++) {
+        let ticker = tickers[i].symbol;
+
+        promises.push(axios.get(DATA_API_URL + ticker.replace('^', '.').replace('/', '.').toLowerCase()));
+
+        if ((promises.length !== 0 && i % 50 === 0) || i === tickers.length - 1) {
+            console.log(`progress:  ${i}/${tickers.length}`);
+            await resolvePromises(promises, exchange);
+            promises = [];
+        }
+    }
+
+}
+
+async function resolvePromises(promises, exchange) {
+
+    await Promise.allSettled(promises).then(async (values) => {
+
+        for (let res of values) {
+            let ticker = res.value.config.url.split('t=')[1];
+            if (res.value.data !== '') {
+                const dom = new JSDOM(res.value.data, {
+                    runScripts: "dangerously",
+                    virtualConsole: new jsdom.VirtualConsole()
+                });
+                await csvWrite(ticker.toUpperCase(), exchange, dom.window.dataDaily);
+            } else console.log('error:', ticker);
+        }
+
+    });
+
+}
+
+async function csvWrite(ticker, exchange, records) {
+
+    const csvWriter = createCsvWriter({
+        path: `./tickers/${exchange}/${ticker}.csv`,
+        header: [
+            {id: 'd', title: 'DATE'},
+            {id: 'o', title: 'OPEN'},
+            {id: 'h', title: 'HIGH'},
+            {id: 'l', title: 'LOW'},
+            {id: 'c', title: 'CLOSE'},
+        ]
+    });
+
+    await csvWriter.writeRecords(records).catch(() => console.log('error:', ticker));
+
+}
+
+main();
